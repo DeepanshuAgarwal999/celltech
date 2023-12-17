0d85360306aefa85dfddd75e9959b30c2dce0903
deepanshu agarwal <95143999+DeepanshuAgarwal999@users.noreply.github.com>
2023-12-17 13:00:45 +0530

    Update Destination.tsx

--- src/components/Destination.tsx
+++ src/components/Destination.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import  { useEffect, useState } from "react";
 import { DataType, dummyData } from "../dummyData";
 import { useParams } from "react-router-dom";
 import Navbar from "./Navbar";
