74a7d9ff77add9493bc97d0449fec88b11baaacd
deepanshu agarwal <95143999+DeepanshuAgarwal999@users.noreply.github.com>
2023-12-17 13:00:02 +0530

    Update Cart.tsx

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import  { useEffect } from "react";
 import { DataType, dummyData } from "../dummyData";
 import { useNavigate, useParams } from "react-router-dom";
 import Card from "./Card";
